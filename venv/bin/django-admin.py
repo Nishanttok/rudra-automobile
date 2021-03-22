07b01aac2e3611cf518e3db7044b56a95cb46fda
Nishanttok <nk91172@gmail.com>
2021-03-22 19:15:38 +0530

    First commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/abhir/Documents/WorkSpace/Rudra_automobile/car_dealership/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
